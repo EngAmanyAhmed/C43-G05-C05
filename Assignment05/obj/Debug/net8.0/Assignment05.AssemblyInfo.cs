7a22dc397f1f34c2e86d253b8e6caedee1b1ec9d
Amany Ahmed <eng.amanyahmed.81@gmail.com>
2024-12-28 12:49:34 +0200

    edit branch

--- Assignment05/obj/Debug/net8.0/Assignment05.AssemblyInfo.cs
+++ Assignment05/obj/Debug/net8.0/Assignment05.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e66928b6a559368ed7c2861d118f798c30ff489")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
